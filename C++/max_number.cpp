e3d6f9c4d93d5a9a355e4be85f7614ed976b2eb1
jacketdembys <jacketdembys@yahoo.fr>
2020-02-29 21:00:10 -0600

    program to find the max of 4 numbers in C++

--- C++/max_number.cpp
+++ C++/max_number.cpp
@@ -2,10 +2,6 @@ nclude <iostream>
 #include <cstdio>
 using namespace std;
 
-/*
- * Add `int max_of_four(int a, int b, int c, int d)` here.
- * */
-
 int max_of_four(int a, int b, int c, int d){
   int max_value;
   if ((a>b) && (a>c) && (a>d)){
