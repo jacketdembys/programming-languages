e512eb822bc40d1acc710c82fc76a104685fbf6a
jacketdembys <jacketdembys@yahoo.fr>
2020-03-14 13:07:03 -0500

    C directory created

---
+++ C/test.c
@@ -0,0 +1,14 @@
+// include the necessary libraries
+#include <stdio.h>
+
+int main(){
+  
+  // my name
+  char name[10] = "Jacket";
+
+  // just print something 
+  printf("Hello, I am %s. Nice to meet you\n", name);
+
+  // return success
+  return 0;
+}
